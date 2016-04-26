46d78889727af6a2a04200f7ad831493f3992521
Anton <antonbergenudd@hotmail.com>
2016-04-27 00:16:46 +0200

    db stable

--- js/index.js
+++ js/index.js
@@ -1,53 +1,54 @@
 var ref = new Firebase('https://pushtest2.firebaseio.com/');
+var deviceID;
 ref.once("value", function(snapshot){
-    console.log(snapshot.val());
-})
-var pubnub = PUBNUB.init({
-    subscribe_key: 'sub-c-ee2ffedc-0bef-11e6-b422-0619f8945a4f',
-    publish_key:   'pub-c-8493cf03-067c-44d2-a665-b307882c6c4a',
-});
+    deviceID = snapshot.val().devices;
+    var pubnub = PUBNUB.init({
+        subscribe_key: 'sub-c-ee2ffedc-0bef-11e6-b422-0619f8945a4f',
+        publish_key:   'pub-c-8493cf03-067c-44d2-a665-b307882c6c4a',
+    });
 
-var ref = new Firebase('https://pushtest2.firebaseio.com/');
+    var ref = new Firebase('https://pushtest2.firebaseio.com/');
 
-changeTemperature();
+    changeTemperature();
 
-function changeTemperature(e) {
-    var temp = 90;
-  
-    pubnub.publish({
-        channel: 'gcm-test',
-        message: {
-            temperature: temp
+    function changeTemperature(e) {
+        var temp = 90;
+      
+        pubnub.publish({
+            channel: 'gcm-test',
+            message: {
+                temperature: temp
+            }
+        });
+
+        if(temp >= 80) {
+            sendPush();
         }
-    });
-
-    if(temp >= 80) {
-        sendPush();
     }
-}
 
-function sendPush() {
-    pubnub.mobile_gw_provision ({
-        device_id: 'APA91bERgbCFiAaR5awbHISeMDlCYfJB7pe95StxP8zNETEkBxgWY-HkxTXkB....', // Reg ID you got on your device
-        channel  : 'gcm-test',
-        op: 'add', 
-        gw_type: 'gcm',
-        error : function(msg){console.log(msg);},
-        callback : successCallback
-    });
-}
+    function sendPush() {
+        pubnub.mobile_gw_provision ({
+            device_id: deviceID, // Reg ID you got on your device
+            channel  : 'gcm-test',
+            op: 'add', 
+            gw_type: 'gcm',
+            error : function(msg){console.log(msg);},
+            callback : successCallback
+        });
+    }
 
-function successCallback() {
-    var message = PNmessage();
+    function successCallback() {
+        var message = PNmessage();
 
-    message.pubnub = pubnub;
-    message.callback = function(msg){ console.log(msg); };
-    message.error = function (msg){ console.log(msg); };
-    message.channel = 'gcm-test';
-    message.gcm = {
-        title: 'Push Demo',
-        message: 'The room temperature is set too high'
-    };
+        message.pubnub = pubnub;
+        message.callback = function(msg){ console.log(msg); };
+        message.error = function (msg){ console.log(msg); };
+        message.channel = 'gcm-test';
+        message.gcm = {
+            title: 'Push Demo',
+            message: 'The room temperature is set too high'
+        };
 
-    message.publish();
-}
+        message.publish();
+    }
+})
